b9c765a5709fe22087300e7c2866942b2d2ea24d
Sebastien Le Marchand <sebastien.lemarchand@gmail.com>
2018-11-23 23:39:24 +0100

    Fixes & minor improvements

--- page-position-web/src/main/resources/META-INF/resources/page_position.js
+++ page-position-web/src/main/resources/META-INF/resources/page_position.js
@@ -20,11 +20,11 @@
 		$items.on("dragstart", null, onDragStart);
 	
 		$items.on("dragover", null, onDragOver);
+		
+		$items.on("dragleave", null, onDragLeave);
 	
 		$items.on("drop", null, onDrop);
 		
-		$items.on( "dragleave", null, resetCssClasses);
-	
 		addStyleTag();
 	}
 	
@@ -33,7 +33,8 @@
 		var id = 'page_position_css';
 		var $style = $('#' + id);
 		if($style.length == 0) {
-			var tag = '<link id="' + id +'"href="/o/page_position/page_position.css" rel="stylesheet" type="text/css">';
+			var timestamp = /.*t=([0-9]*)/.exec($('script[src^="/o/page_position/page_position.js"]').attr('src'))[1];
+			var tag = '<link id="' + id +'"href="/o/page_position/page_position.css?t=' + timestamp + '" rel="stylesheet" type="text/css">';
 			$('head').append(tag);
 		}
 	}
@@ -41,10 +42,18 @@
 	function onDragStart(event) {
 	
 	   var plid = getPlid(event.currentTarget);
+	   
+	   var priority = getPriority(event.currentTarget);
+	   
+	   var columnIndex = getColumnIndex(event.currentTarget);
 	
 	   var dataTransfer = event.originalEvent.dataTransfer;
 	
 	   dataTransfer.setData("plid", plid);
+	   
+	   dataTransfer.setData("priority", priority);
+	   
+	   dataTransfer.setData("columnIndex", columnIndex);
 	   
 	   window.__page_position__last_dragged_element = event.currentTarget;
 	}
@@ -59,7 +68,7 @@
 		
 			var cssClass = 'dragover-' + position;
 		
-			var $target = $(event.currentTarget);
+			var $target = get$ItemElement(event.currentTarget);
 			
 			if(!$target.hasClass(cssClass)) {
 				resetCssClasses();
@@ -68,28 +77,60 @@
 		}
 	}
 	
-	function onDrop(event) {
+	function onDragLeave(event) {
+		
+		console.log('page_position.js::onDragLeave()');
+		
+		window.__page_position__position = getDragOverPosition(event);
+		
+		window.__page_position__targetPlid = getPlid(event.currentTarget);
 	
-		event.originalEvent.preventDefault();
-	
+		window.__page_position__targetPriority = getPriority(event.currentTarget);	
+		
+		window.__page_position__targetColumnIndex = getColumnIndex(event.currentTarget);	
+		
 		resetCssClasses();
+	}
 	
-		var position = getDragOverPosition(event);
+	function onDrop(event) {
+		
+		console.log('page_position.js::onDrop()');
+		
+		event.originalEvent.preventDefault();
+		
+		var position = window.__page_position__position;
+		
+		var targetPlid = window.__page_position__targetPlid;
 	
-		var plid = event.originalEvent.dataTransfer.getData("plid");
+		var targetPriority = window.__page_position__targetPriority;
+		
+		var targetColumnIndex = window.__page_position__targetColumnIndex;
+		
+		var sourcePlid = event.originalEvent.dataTransfer.getData("plid");
+		
+		var sourcePriority = event.originalEvent.dataTransfer.getData("priority");
+		
+		var sourceColumnIndex = event.originalEvent.dataTransfer.getData("columnIndex");
 	  	
-		var targetPlid = getPlid(event.currentTarget);
-	
-		var targetPriority = getPriority(event.currentTarget);
-	
-		if(plid === targetPlid ) {
+		if(sourcePlid === targetPlid ) {
 			return;
+		}
+		
+		/*
+		console.log(sourceColumnIndex);
+		console.log(targetColumnIndex);
+		console.log(sourcePriority);
+		console.log(targetPriority);
+		*/
+		
+		if((sourceColumnIndex == targetColumnIndex) && (sourcePriority < targetPriority)) {
+			targetPriority -= 1;
 		}
 
 		var pAuth = Liferay.authToken;
 	
 		var data = {
-			plid: plid,
+			plid: sourcePlid,
 			p_auth: pAuth
 		};
 	
@@ -104,6 +145,8 @@
 			data.priority = 0;
 		}
 	
+		console.log(data);
+		
 	  	$.post({
 	  		url: '/c/portal/page_position/edit',
 	  		data: data,
@@ -113,8 +156,7 @@
 	  		},
 	  	}).fail(function(response) {
 	  		console.log('fail: ' + response.getResponseHeader('X-Error-Message'));
-  			resetCssClasses();
-  		});	
+  		});
 	}
 	
 	function isValidDropTarget(target) {
@@ -164,31 +206,64 @@
 	
 	function getHref(element) {
 		
-	   var href = $(element)
-			.find('a.dropdown-item[href*="GroupPagesPortlet_mvcRenderCommandName=%2Flayout%2Fedit_layout"]')
-			.attr('href');
-	
-	   return href;
+		var $e = $(element);
+		
+		var href = $e
+				.find('a.dropdown-item[href*="GroupPagesPortlet_mvcRenderCommandName=%2Flayout%2Fedit_layout"]')
+				.attr('href');
+		
+		return href;
 	}
 	
 	function getPriority(element) {
 		
-		return $(element).parent().find('> li').index(element);
+		return get$ItemElement(element).parent().find('> li').index(element);
+	}
+	
+	function getColumnIndex(element) {
+		var $column = get$ItemElement(element).parent();
+		return $column.parent().find('> ul').index($column);
+	}
+	
+	function get$ItemElement(element) {
+		var $itemElement = $(element);
+		
+		if(!$itemElement.hasClass('list-group-item')) {
+			$itemElement = $itemElement.parent('.list-group-item');
+		}
+		
+		return  $itemElement;
 	}
 	
 	function getDragOverPosition(event) {
+		
 		var height = event.currentTarget.clientHeight; 
-		var ratio = 1.0 / 6.0;
+		var ratio = 1.0 / 4.0;
 		var topY = height * ratio;
 		var bottomY = height - (height * ratio);
-		if(event.offsetY < topY) { 
+		var currentY = event.pageY - $(event.currentTarget).offset().top;
+		
+		if(currentY < topY) { 
 			p = 'top';
-		} else if(event.offsetY > bottomY){
+		} else if(currentY > bottomY){
 			p = 'bottom'; 
 		} else { 
 			p = 'middle';
 		};
 	
+		if(window._currentY === undefined || window._currentY !== currentY) {
+			window._currentY = currentY;
+			
+			/*
+			console.log(getPriority(event.currentTarget));
+			console.log("__");
+			console.log('currentY: ' + currentY);
+			console.log('topY: ' + topY);
+			console.log('bottomY: ' + bottomY);
+			console.log('position: ' + p);
+			*/
+		}
+		
 		return p;
 	}
 	
